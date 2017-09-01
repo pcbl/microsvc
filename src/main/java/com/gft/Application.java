a84d598f03e0047d1ff38a6bca240c175e44e765
Pedro Castelo Branco Lourenco <pedro_castelo@hotmail.com>
2017-09-01 14:29:16 +0200

    testing docker...

--- src/main/java/com/gft/Application.java
+++ src/main/java/com/gft/Application.java
@@ -2,15 +2,28 @@ package com.gft;
 
 import com.gft.model.Product;
 import com.gft.repositories.InMemoryRepository;
+import com.gft.repositories.MongoRepository;
 import com.gft.service.ProductService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws IOException {
+
+        //  ProductService.repository = SetupMongoRepository();
         ProductService.repository = SetupInMemoryRepository();
+        SpringApplication.run(Application.class, args);
+
+    }
+
+    private static MongoRepository<Product> SetupMongoRepository() throws IOException {
+        MongoRepository<Product> toReturn = new MongoRepository<>("Microservice77","Product");
+
+        return toReturn;
     }
 
     private static InMemoryRepository<Product> SetupInMemoryRepository()
--- src/main/java/com/gft/repositories/MongoRepository.java
+++ src/main/java/com/gft/repositories/MongoRepository.java
@@ -1,4 +1,46 @@
 package com.gft.repositories;
 
-public class MongoRepository {
+import com.google.gson.Gson;
+import com.mongodb.*;
+import com.mongodb.util.JSON;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class MongoRepository<T extends Entity> implements IRepository<T> {
+
+    private DBCollection items;
+
+    public MongoRepository(String databaseName, String collectionName) throws IOException {
+    /*    Properties settings = new Properties();
+        settings.load(new FileInputStream("src/main/resources/application.properties"));
+        String mongoURI = settings.getProperty("mongodb.uri");
+        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://pcbl:oL91uQU8R0VzBnhI6T97QDxk0u5Zip3K02wSonncUWb9DVFM7nhEfvA1dmoefRKUphCYbuiY50bWYS9KeKgH3A==@pcbl.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"));
+        DB database = mongoClient.getDB(databaseName);
+        items = database.getCollection(collectionName);*/
+    }
+
+    @Override
+    public Iterable<T> Items() {
+     return null;
+    }
+
+    @Override
+    public void Add(T instance) {
+        Gson gson = new Gson();
+        BasicDBObject dbObject = (BasicDBObject) JSON.parse(gson.toJson(instance));
+        items.insert(dbObject);
+    }
+
+    @Override
+    public T GetById(long id) {
+        return null;
+    }
 }
