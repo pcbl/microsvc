ae431ac518bf2a3221e51a5d3da38ed678abf55b
Pedro Castelo Branco Lourenco <pedro_castelo@hotmail.com>
2017-09-05 16:15:03 +0200

    Adding Swagger support

--- src/main/java/com/gft/ApplicationConfig.java
+++ src/main/java/com/gft/ApplicationConfig.java
@@ -1,20 +1,45 @@
 package com.gft;
 
 import javax.inject.Named;
+import javax.ws.rs.ApplicationPath;
+
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
 @Configuration
 public class ApplicationConfig {
-    @Named
-    static class JerseyConfig extends ResourceConfig {
+    @Component
+    @ApplicationPath("/api")
+    public class JerseyConfig extends ResourceConfig {
         public JerseyConfig() {
-            //The this.packages is failig...
+            //The this.packages is failing...
             //Reason:
             //https://stackoverflow.com/questions/42194680/spring-boot-application-wont-run-when-trying-to-run-from-the-jar-file
             //https://github.com/spring-projects/spring-boot/issues/6598
             //https://github.com/spring-projects/spring-boot/issues/1468
             //this.packages("com.gft.service");
-            this.register(com.gft.service.ProductService.class);
+            register(com.gft.service.ProductService.class);
+            configureSwagger();
+        }
+
+        private void configureSwagger() {
+            register(ApiListingResource.class);
+            register(SwaggerSerializers.class);
+            BeanConfig beanConfig = new BeanConfig();
+            beanConfig.setVersion("0.0.1");
+            beanConfig.setSchemes(new String[]{"http"});
+            beanConfig.setHost("localhost:8080");
+            beanConfig.setBasePath("/");
+            beanConfig.setDescription("Sample");
+            beanConfig.setContact("VIIGit");
+            beanConfig.setResourcePackage("com.gft.service");
+            beanConfig.setPrettyPrint(true);
+            beanConfig.setScan(true);
         }
     }
 }
--- src/main/java/com/gft/service/ProductService.java
+++ src/main/java/com/gft/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.gft.service;
 
+import com.gft.model.NewProductDto;
 import com.gft.model.Product;
 import com.gft.repositories.IRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +15,16 @@ import javax.ws.rs.core.Response;
 
 @Service
 @Path("/Products")
+@Api
 public class ProductService {
     @Autowired
     private IRepository<Product> repository;
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Get List of Products",
+            response = Product.class,
+            responseContainer = "List"
+    )
     public Iterable<Product> getProducts() {
         return repository.Items();
     }
@@ -24,6 +32,9 @@ public class ProductService {
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Get Details of a product, given its Id",
+            response = Product.class
+    )
     public Product getProduct(@PathParam("id") long id) {
         Product product = repository.GetById(id);
         return product;
@@ -32,7 +43,13 @@ public class ProductService {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response addProduct(Product newProduct) {
+    @ApiOperation(value = "Adds a new Product",
+            response = Response.class
+    )
+    public Response addProduct(NewProductDto newProductDto) {
+        Product newProduct = new Product();
+        newProduct.setName(newProductDto.getName());
+        newProduct.setCategory(newProductDto.getCategory());
         repository.Add(newProduct);
         return Response.created(URI.create("/Products/"+newProduct.getId())).build();
     }
